6a6dd383255a6bf8bd7434f7cc55c3808ae3ad94
pocketberserker <pocketberserker@gmail.com>
2018-05-18 12:49:43 +0900

    NoiseFilterを実装

--- packages/akashic-filters/src/index.ts
+++ packages/akashic-filters/src/index.ts
@@ -2,3 +2,4 @@ export * from "./Filter";
 export * from "./FilterContainer";
 export * from "./alpha/AlphaFilter";
 export * from "./colormatrix/ColorMatrixFilter";
+export * from "./noise/NoiseFilter";
---
+++ packages/akashic-filters/src/noise/NoiseFilter.ts
@@ -0,0 +1,88 @@
+/*
+The MIT License
+Copyright (c) 2013-2017 Mathew Groves, Chad Engler
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+// https://github.com/pixijs/pixi.js/blob/v4.7.3/src/filters/noise/NoiseFilter.js
+
+import {Filter} from "../Filter";
+
+const fragmentShader = `#version 100
+precision highp float;
+varying vec2 vTexCoord;
+varying vec4 vColor;
+uniform sampler2D uSampler;
+
+uniform float uNoise;
+uniform float uSeed;
+
+float rand(vec2 co) {
+  return fract(sin(dot(co.xy, vec2(12.9898, 78.233))) * 43758.5453);
+}
+
+void main() {
+  vec4 color = texture2D(uSampler, vTexCoord);
+  float randomValue = rand(gl_FragCoord.xy * uSeed);
+  float diff = (randomValue - 0.5) * uNoise;
+
+  // Un-premultiply alpha before applying the color matrix. See issue #3539.
+  if (color.a > 0.0) {
+    color.rgb /= color.a;
+  }
+
+  color.r += diff;
+  color.g += diff;
+  color.b += diff;
+
+  // Premultiply alpha again.
+  color.rgb *= color.a;
+
+  gl_FragColor = color;
+}`;
+
+export class NoiseFilter implements Filter {
+  private shader: g.ShaderProgram;
+
+  constructor(noise = 0.5, seed = Math.random()) {
+    this.shader = new g.ShaderProgram({
+      fragmentShader,
+      uniforms: {
+        uNoise: {
+          type: "float",
+          value: noise
+        },
+        uSeed: {
+          type: "float",
+          value: seed
+        }
+      }
+    });
+  }
+
+  apply(renderer: g.Renderer): void {
+    renderer.setShaderProgram(this.shader);
+  }
+
+  set noise(value: number) {
+    this.shader.uniforms.uNoise.value = value;
+  }
+
+  set seed(value: number) {
+    this.shader.uniforms.uSeed.value = value;
+  }
+}
